b74cd5056873ced33a5fe7a979a4a9e0666c4268
Dmytro Stadnytskyi <127896355+Xigmanu@users.noreply.github.com>
2025-07-17 20:09:44 +0200

    Fix the namespace name

--- tests/Borm.Provider/Borm.Data.Sql.Sqlite.Test/SqliteStatementFactoryTest.cs
+++ tests/Borm.Provider/Borm.Data.Sql.Sqlite.Test/SqliteStatementFactoryTest.cs
@@ -1,6 +1,6 @@
 ﻿using System.Data;
 
-namespace Borm.Data.Sql.Sqlite.Test;
+namespace Borm.Data.Sql.Sqlite.Tests;
 
 public class SqliteStatementFactoryTest
 {
--- tests/Borm.Provider/Borm.Data.Sql.Sqlite.Test/SqliteTypeHelperTest.cs
+++ tests/Borm.Provider/Borm.Data.Sql.Sqlite.Test/SqliteTypeHelperTest.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Data.Sqlite;
 
-namespace Borm.Data.Sql.Sqlite.Test;
+namespace Borm.Data.Sql.Sqlite.Tests;
 
 public class SqliteTypeHelperTest
 {
