dc0e1e9b5dea1899c29e54495bf7fb636f8f79bb
Dmytro Stadnytskyi <127896355+Xigmanu@users.noreply.github.com>
2025-07-18 23:27:19 +0200

    Create a class that represents a model

---
+++ src/Borm/Schema/EntityModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using Borm.Extensions;
+using Borm.Properties;
+using Borm.Reflection;
+
+namespace Borm.Schema;
+
+public sealed class EntityModel
+{
+    private readonly Assembly _modelSource;
+
+    public EntityModel(Assembly modelSource)
+    {
+        ArgumentNullException.ThrowIfNull(modelSource);
+        _modelSource = modelSource;
+    }
+
+    internal IEnumerable<ReflectedTypeInfo> GetReflectedInfo()
+    {
+        IEnumerable<Type> entityTypes = _modelSource.ExportedTypes.Where(type =>
+            type.HasAttribute<EntityAttribute>()
+        );
+        List<ReflectedTypeInfo> reflectedInfos = new(entityTypes.Count());
+        MetadataParser parser = new();
+        foreach (Type entityType in entityTypes)
+        {
+            if (entityType.IsAbstract)
+            {
+                throw new ArgumentException(
+                    Strings.EntityTypeCannotBeAbstract(entityType.FullName!)
+                );
+            }
+
+            ReflectedTypeInfo reflectedInfo = parser.Parse(entityType);
+            reflectedInfos.Add(reflectedInfo);
+        }
+
+        return reflectedInfos;
+    }
+}
