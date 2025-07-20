Xigmanu/borm

commit 1fc770b5b7e67e0e26a686738a834814f133c2b4

Add entity validation (#5)
* Add validator interface

* Optionally validate entities on insert/update

* Rename namespace and class

* Rename the namespace properly this time

* Fix usings

* Fix more usings

src/Borm/Model/EntityModel.cs
@@ -1,0 +1,62 @@
+﻿using Borm.Extensions;
+using Borm.Properties;
+using Borm.Reflection;
+
+namespace Borm.Model;
+
+public sealed class EntityModel
+{
+    private readonly HashSet<Type> _entities;
+    private readonly Dictionary<Type, Action<object>> _entityValidators;
+
+    public EntityModel()
+    {
+        (_entities, _entityValidators) = ([], []);
+    }
+
+    public void AddEntity(Type entityType)
+    {
+        if (!entityType.HasAttribute<EntityAttribute>())
+        {
+            throw new ArgumentException(Strings.NotDecoratedEntityType(), nameof(entityType));
+        }
+        _entities.Add(entityType);
+    }
+
+    public void AddEntity<TEntity>(Type entityType, IEntityValidator<TEntity> validator)
+    {
+        AddEntity(entityType);
+        _entityValidators[typeof(TEntity)] = WrapValidator(validator);
+    }
+
+    internal IEnumerable<ReflectedTypeInfo> GetReflectedInfo()
+    {
+        List<ReflectedTypeInfo> reflectedInfos = new(_entities.Count);
+        MetadataParser parser = new();
+        foreach (Type entityType in _entities)
+        {
+            if (entityType.IsAbstract)
+            {
+                throw new ArgumentException(
+                    Strings.EntityTypeCannotBeAbstract(entityType.FullName!)
+                );
+            }
+
+            ReflectedTypeInfo reflectedInfo = parser.Parse(entityType);
+            reflectedInfos.Add(reflectedInfo);
+        }
+
+        return reflectedInfos;
+    }
+
+    internal Action<object>? GetValidatorFunc(Type entityType)
+    {
+        _ = _entityValidators.TryGetValue(entityType, out Action<object>? validator);
+        return validator;
+    }
+
+    private static Action<object> WrapValidator<TEntity>(IEntityValidator<TEntity> validator)
+    {
+        return (obj) => validator.Validate((TEntity)obj);
+    }
+}

src/Borm/Properties/Strings.Designer.cs
@@ -93,6 +93,14 @@ public static class Strings
     }
 
     /// <summary>
+    /// Entity types must be decorated with the 'EntityAttribute'.
+    /// </summary>
+    public static string NotDecoratedEntityType()
+    {
+        return GetString(formatArgs: null);
+    }
+
+    /// <summary>
     /// Missing SQL parameter for required column '{columnName}' in table '{tableName}'.
     /// </summary>
     public static string SqlStatementParameterRowMapping(string columnName, string tableName)

src/Borm/Properties/Strings.resx
@@ -147,6 +147,9 @@
   <data name="MissingTableForEntity" xml:space="preserve">
     <value>The data context does not contain a table for the entity of type '{0}'.</value>
   </data>
+  <data name="NotDecoratedEntityType" xml:space="preserve">
+    <value>Entity types must be decorated with the 'EntityAttribute'.</value>
+  </data>
   <data name="SqlStatementParameterRowMapping" xml:space="preserve">
     <value>Missing SQL parameter for required column '{1}' in table '{0}'.</value>
   </data>

src/Borm/Schema/EntityModel.cs
@@ -1,40 +1,0 @@
-﻿using System.Reflection;
-using Borm.Extensions;
-using Borm.Properties;
-using Borm.Reflection;
-
-namespace Borm.Schema;
-
-public sealed class EntityModel
-{
-    private readonly IEnumerable<Type> _types;
-
-    public EntityModel(IEnumerable<Type> types)
-    {
-        ArgumentNullException.ThrowIfNull(types);
-        _types = types;
-    }
-
-    internal IEnumerable<ReflectedTypeInfo> GetReflectedInfo()
-    {
-        IEnumerable<Type> entityTypes = _types.Where(type =>
-            type.HasAttribute<EntityAttribute>()
-        );
-        List<ReflectedTypeInfo> reflectedInfos = new(entityTypes.Count());
-        MetadataParser parser = new();
-        foreach (Type entityType in entityTypes)
-        {
-            if (entityType.IsAbstract)
-            {
-                throw new ArgumentException(
-                    Strings.EntityTypeCannotBeAbstract(entityType.FullName!)
-                );
-            }
-
-            ReflectedTypeInfo reflectedInfo = parser.Parse(entityType);
-            reflectedInfos.Add(reflectedInfo);
-        }
-
-        return reflectedInfos;
-    }
-}

commit a799b1fdee7d01cded5f473715316418cede8aa4

Wrap validator exceptions introduced in 1fc770b5b7e67e0e26a686738a834814f133c2b4

src/Borm/Model/EntityModel.cs
@@ -57,6 +57,19 @@ public sealed class EntityModel
 
     private static Action<object> WrapValidator<TEntity>(IEntityValidator<TEntity> validator)
     {
-        return (obj) => validator.Validate((TEntity)obj);
+        return (obj) =>
+        {
+            try
+            {
+                validator.Validate((TEntity)obj);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(
+                    Strings.EntityValidationFailed(typeof(TEntity)),
+                    ex
+                );
+            }
+        };
     }
 }

src/Borm/Properties/Strings.Designer.cs
@@ -37,6 +37,14 @@ public static class Strings
     }
 
     /// <summary>
+    /// Validation failed for entity type '{entityType}'.
+    /// </summary>
+    public static string EntityValidationFailed(Type entityType)
+    {
+        return GetString([entityType]);
+    }
+
+    /// <summary>
     /// Column index cannot be lesser than zero.
     /// </summary>
     public static string InvalidColumnIndex()

src/Borm/Properties/Strings.resx
@@ -126,6 +126,9 @@
   <data name="EntityTypeCannotBeAbstract" xml:space="preserve">
     <value>The entity type '{0}' cannot be abstract.</value>
   </data>
+  <data name="EntityValidationFailed" xml:space="preserve">
+    <value>Validation failed for entity type '{0}'.</value>
+  </data>
   <data name="InvalidColumnIndex" xml:space="preserve">
     <value>Column index cannot be lesser than zero.</value>
   </data>