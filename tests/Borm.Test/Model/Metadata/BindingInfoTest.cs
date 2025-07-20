Xigmanu/borm

commit 1fc770b5b7e67e0e26a686738a834814f133c2b4

Add entity validation (#5)
* Add validator interface

* Optionally validate entities on insert/update

* Rename namespace and class

* Rename the namespace properly this time

* Fix usings

* Fix more usings

src/Borm/Data/BormDataAdapter.cs
@@ -1,7 +1,7 @@
 ﻿using System.Data;
 using System.Diagnostics;
 using Borm.Data.Sql;
-using Borm.Schema.Metadata;
+using Borm.Model.Metadata;
 
 namespace Borm.Data;
 

src/Borm/Model/EntityModel.cs
@@ -1,0 +1,62 @@
+﻿using Borm.Extensions;
+using Borm.Properties;
+using Borm.Reflection;
+
+namespace Borm.Model;
+
+public sealed class EntityModel
+{
+    private readonly HashSet<Type> _entities;
+    private readonly Dictionary<Type, Action<object>> _entityValidators;
+
+    public EntityModel()
+    {
+        (_entities, _entityValidators) = ([], []);
+    }
+
+    public void AddEntity(Type entityType)
+    {
+        if (!entityType.HasAttribute<EntityAttribute>())
+        {
+            throw new ArgumentException(Strings.NotDecoratedEntityType(), nameof(entityType));
+        }
+        _entities.Add(entityType);
+    }
+
+    public void AddEntity<TEntity>(Type entityType, IEntityValidator<TEntity> validator)
+    {
+        AddEntity(entityType);
+        _entityValidators[typeof(TEntity)] = WrapValidator(validator);
+    }
+
+    internal IEnumerable<ReflectedTypeInfo> GetReflectedInfo()
+    {
+        List<ReflectedTypeInfo> reflectedInfos = new(_entities.Count);
+        MetadataParser parser = new();
+        foreach (Type entityType in _entities)
+        {
+            if (entityType.IsAbstract)
+            {
+                throw new ArgumentException(
+                    Strings.EntityTypeCannotBeAbstract(entityType.FullName!)
+                );
+            }
+
+            ReflectedTypeInfo reflectedInfo = parser.Parse(entityType);
+            reflectedInfos.Add(reflectedInfo);
+        }
+
+        return reflectedInfos;
+    }
+
+    internal Action<object>? GetValidatorFunc(Type entityType)
+    {
+        _ = _entityValidators.TryGetValue(entityType, out Action<object>? validator);
+        return validator;
+    }
+
+    private static Action<object> WrapValidator<TEntity>(IEntityValidator<TEntity> validator)
+    {
+        return (obj) => validator.Validate((TEntity)obj);
+    }
+}

src/Borm/Model/Metadata/ConversionBinding.cs
@@ -1,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace Borm.Model.Metadata;
+
+internal sealed class ConversionBinding
+{
+    public static readonly ConversionBinding Empty = new(
+        (v) => throw new NotImplementedException(),
+        (o) => throw new NotImplementedException()
+    );
+
+    public ConversionBinding(
+        Func<ValueBuffer, object> materializeEntity,
+        Func<object, ValueBuffer> convertToValueBuffer
+    )
+    {
+        MaterializeEntity = materializeEntity;
+        ConvertToValueBuffer = convertToValueBuffer;
+    }
+
+    internal Func<object, ValueBuffer> ConvertToValueBuffer { get; }
+
+    internal Func<ValueBuffer, object> MaterializeEntity { get; }
+}

src/Borm/Properties/Strings.Designer.cs
@@ -93,6 +93,14 @@ public static class Strings
     }
 
     /// <summary>
+    /// Entity types must be decorated with the 'EntityAttribute'.
+    /// </summary>
+    public static string NotDecoratedEntityType()
+    {
+        return GetString(formatArgs: null);
+    }
+
+    /// <summary>
     /// Missing SQL parameter for required column '{columnName}' in table '{tableName}'.
     /// </summary>
     public static string SqlStatementParameterRowMapping(string columnName, string tableName)

src/Borm/Properties/Strings.resx
@@ -147,6 +147,9 @@
   <data name="MissingTableForEntity" xml:space="preserve">
     <value>The data context does not contain a table for the entity of type '{0}'.</value>
   </data>
+  <data name="NotDecoratedEntityType" xml:space="preserve">
+    <value>Entity types must be decorated with the 'EntityAttribute'.</value>
+  </data>
   <data name="SqlStatementParameterRowMapping" xml:space="preserve">
     <value>Missing SQL parameter for required column '{1}' in table '{0}'.</value>
   </data>

tests/Borm.Test/Model/Metadata/BindingInfoTest.cs
@@ -1,0 +1,5 @@
+﻿namespace Borm.Tests.Model.Metadata;
+public sealed class BindingInfoTest
+{
+
+}

src/Borm/Schema/EntityModel.cs
@@ -1,40 +1,0 @@
-﻿using System.Reflection;
-using Borm.Extensions;
-using Borm.Properties;
-using Borm.Reflection;
-
-namespace Borm.Schema;
-
-public sealed class EntityModel
-{
-    private readonly IEnumerable<Type> _types;
-
-    public EntityModel(IEnumerable<Type> types)
-    {
-        ArgumentNullException.ThrowIfNull(types);
-        _types = types;
-    }
-
-    internal IEnumerable<ReflectedTypeInfo> GetReflectedInfo()
-    {
-        IEnumerable<Type> entityTypes = _types.Where(type =>
-            type.HasAttribute<EntityAttribute>()
-        );
-        List<ReflectedTypeInfo> reflectedInfos = new(entityTypes.Count());
-        MetadataParser parser = new();
-        foreach (Type entityType in entityTypes)
-        {
-            if (entityType.IsAbstract)
-            {
-                throw new ArgumentException(
-                    Strings.EntityTypeCannotBeAbstract(entityType.FullName!)
-                );
-            }
-
-            ReflectedTypeInfo reflectedInfo = parser.Parse(entityType);
-            reflectedInfos.Add(reflectedInfo);
-        }
-
-        return reflectedInfos;
-    }
-}

src/Borm/Schema/Metadata/ConversionBinding.cs
@@ -1,24 +1,0 @@
-﻿using System.Diagnostics;
-
-namespace Borm.Schema.Metadata;
-
-internal sealed class ConversionBinding
-{
-    public static readonly ConversionBinding Empty = new(
-        (v) => throw new NotImplementedException(),
-        (o) => throw new NotImplementedException()
-    );
-
-    public ConversionBinding(
-        Func<ValueBuffer, object> materializeEntity,
-        Func<object, ValueBuffer> convertToValueBuffer
-    )
-    {
-        MaterializeEntity = materializeEntity;
-        ConvertToValueBuffer = convertToValueBuffer;
-    }
-
-    internal Func<object, ValueBuffer> ConvertToValueBuffer { get; }
-
-    internal Func<ValueBuffer, object> MaterializeEntity { get; }
-}

commit 5c98783df3ec1554429b591522fec89b07ae8899

Add command batching (#6)
* Wrap validator exceptions introduced in 1fc770b5b7e67e0e26a686738a834814f133c2b4

* Execute multiple write statements on the same table as a batch

* Remove test because moq cannot mock DbCommand.Parameters property

* Move batch queuing logic to a separate class

* Use interfaces again

* Remove unused using

* Add tests

src/Borm.Providers/Borm.Data.Sql.Sqlite/SqliteCommandExecutor.cs
@@ -23,8 +23,22 @@ public sealed class SqliteCommandExecutor : IDbStatementExecutor
 
         try
         {
-            statement.PrepareCommand(command);
-            _ = command.ExecuteNonQuery();
+            statement.Prepare(command);
+
+            ParameterBatchQueue batchQueue = statement.BatchQueue;
+            if (batchQueue.Count > 0)
+            {
+                while (batchQueue.Next())
+                {
+                    batchQueue.SetParameterValues(command);
+                    _ = command.ExecuteNonQuery();
+                }
+            }
+            else
+            {
+                _ = command.ExecuteNonQuery();
+            }
+
             transaction.Commit();
         }
         catch
@@ -47,7 +61,7 @@ public sealed class SqliteCommandExecutor : IDbStatementExecutor
         connection.Open();
         try
         {
-            statement.PrepareCommand(command);
+            statement.Prepare(command);
             dataReader = command.ExecuteReader();
         }
         catch

src/Borm/Data/BormDataAdapter.cs
@@ -52,18 +52,40 @@ internal sealed class BormDataAdapter
         for (int i = 0; i < sorted.Length; i++)
         {
             DataTable table = dataSet.Tables[sorted[i].Name]!;
-            List<SqlStatement> statements = CreateUpdateStatements((NodeDataTable)table);
-            statements.ForEach(_executor.ExecuteNonQuery);
+            IEnumerable<SqlStatement> statements = CreateUpdateStatements((NodeDataTable)table);
+            foreach (SqlStatement statement in statements)
+            {
+                _executor.ExecuteNonQuery(statement);
+            }
         }
     }
 
-    private List<SqlStatement> CreateUpdateStatements(NodeDataTable table)
+    private static SqlStatement GetOrCreateSqlStatement(
+        DataTable changes,
+        DataRow row,
+        Dictionary<DataRowState, SqlStatement> rowStateStatements,
+        Func<DataTable, SqlStatement> factoryMethod
+    )
+    {
+        if (rowStateStatements.TryGetValue(row.RowState, out SqlStatement? cached))
+        {
+            return cached;
+        }
+
+        SqlStatement statement = factoryMethod(changes);
+        rowStateStatements[row.RowState] = statement;
+        return statement;
+    }
+
+    private Dictionary<DataRowState, SqlStatement>.ValueCollection CreateUpdateStatements(
+        NodeDataTable table
+    )
     {
         DataTable? changes = table.GetChanges();
-        List<SqlStatement> statements = [];
+        Dictionary<DataRowState, SqlStatement> rowStateStatements = [];
         if (changes == null)
         {
-            return statements;
+            return rowStateStatements.Values;
         }
 
         foreach (DataRow row in changes.Rows)
@@ -78,13 +100,28 @@ internal sealed class BormDataAdapter
             switch (row.RowState)
             {
                 case DataRowState.Added:
-                    statement = _statementFactory.NewInsertStatement(changes);
+                    statement = GetOrCreateSqlStatement(
+                        changes,
+                        row,
+                        rowStateStatements,
+                        _statementFactory.NewInsertStatement
+                    );
                     break;
                 case DataRowState.Modified:
-                    statement = _statementFactory.NewUpdateStatement(changes);
+                    statement = GetOrCreateSqlStatement(
+                        changes,
+                        row,
+                        rowStateStatements,
+                        _statementFactory.NewUpdateStatement
+                    );
                     break;
                 case DataRowState.Deleted:
-                    statement = _statementFactory.NewDeleteStatement(changes);
+                    statement = GetOrCreateSqlStatement(
+                        changes,
+                        row,
+                        rowStateStatements,
+                        _statementFactory.NewDeleteStatement
+                    );
                     deletedRowClone = ((BormDataSet)table.DataSet!).GetDeletedRowClone(
                         table,
                         changes.Rows.IndexOf(row)
@@ -95,11 +132,9 @@ internal sealed class BormDataAdapter
             }
 
             Debug.Assert(!string.IsNullOrEmpty(statement.Sql));
-            statement.SetParameters(deletedRowClone ?? row);
-
-            statements.Add(statement);
+            statement.BatchQueue.AddFromRow(deletedRowClone ?? row);
         }
 
-        return statements;
+        return rowStateStatements.Values;
     }
 }

src/Borm/Data/Sql/ParameterBatchQueue.cs
@@ -1,0 +1,47 @@
+﻿using System.Data;
+using System.Diagnostics;
+
+namespace Borm.Data.Sql;
+
+public sealed class ParameterBatchQueue
+{
+    private readonly List<object?[]> _values;
+    private int _currentIdx;
+
+    public ParameterBatchQueue()
+    {
+        _values = [];
+        _currentIdx = -1;
+    }
+
+    public int Count => _values.Count;
+
+    public bool Next()
+    {
+        _currentIdx++;
+        return _values.Count > _currentIdx;
+    }
+
+    public void SetParameterValues(IDbCommand dbCommand)
+    {
+        object?[] values = _values[_currentIdx];
+        for (int i = 0; i < values.Length; i++)
+        {
+            IDbDataParameter? param = dbCommand.Parameters[i] as IDbDataParameter;
+            Debug.Assert(param != null);
+            param.Value = values[i];
+        }
+    }
+
+    internal void AddFromRow(DataRow row)
+    {
+        DataTable table = row.Table;
+        DataColumnCollection columns = table.Columns;
+        object?[] values = new object[columns.Count];
+        for (int i = 0; i < values.Length; i++)
+        {
+            values[i] = row[i];
+        }
+        _values.Add(values);
+    }
+}

src/Borm/Data/Sql/SqlStatement.cs
@@ -1,7 +1,5 @@
 ﻿using System.Data;
 using System.Data.Common;
-using System.Runtime.CompilerServices;
-using Borm.Properties;
 
 namespace Borm.Data.Sql;
 
@@ -15,50 +13,22 @@ public sealed class SqlStatement
     {
         _sql = sql;
         _parameters = parameters;
+        BatchQueue = new();
     }
 
+    public ParameterBatchQueue BatchQueue { get; }
     public DbParameter[] Parameters => _parameters;
     public string Sql => _sql;
 
-    public void PrepareCommand(IDbCommand dbCommand)
+    public void Prepare(IDbCommand dbCommand)
     {
         dbCommand.CommandText = _sql;
         IDataParameterCollection cmdParameters = dbCommand.Parameters;
+        cmdParameters.Clear();
         for (int i = 0; i < _parameters.Length; i++)
         {
-            if (cmdParameters.Contains(_parameters[i].ParameterName))
-            {
-                cmdParameters[i] = _parameters[i].Value;
-            }
-            else
-            {
-                cmdParameters.Add(_parameters[i]);
-            }
+            cmdParameters.Add(_parameters[i]);
         }
         dbCommand.Prepare();
-    }
-
-    internal void SetParameters(DataRow row)
-    {
-        DataTable table = row.Table;
-        DataColumnCollection columns = table.Columns;
-        for (int i = 0; i < Parameters.Length; i++)
-        {
-            DbParameter current = Parameters[i];
-            string columnName = GetColumnNameFromParameterName(current.ParameterName);
-            if (columns[columnName] == null)
-            {
-                throw new InvalidOperationException(
-                    Strings.SqlStatementParameterRowMapping(columnName, table.TableName)
-                );
-            }
-            current.Value = row[columnName];
-        }
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static string GetColumnNameFromParameterName(string parameterName)
-    {
-        return parameterName[0] == DefaultParameterPrefix ? parameterName[1..] : parameterName;
     }
 }

src/Borm/Model/EntityModel.cs
@@ -57,6 +57,19 @@ public sealed class EntityModel
 
     private static Action<object> WrapValidator<TEntity>(IEntityValidator<TEntity> validator)
     {
-        return (obj) => validator.Validate((TEntity)obj);
+        return (obj) =>
+        {
+            try
+            {
+                validator.Validate((TEntity)obj);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(
+                    Strings.EntityValidationFailed(typeof(TEntity)),
+                    ex
+                );
+            }
+        };
     }
 }

src/Borm/Model/Metadata/ConversionBinding.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-
-namespace Borm.Model.Metadata;
+﻿namespace Borm.Model.Metadata;
 
 internal sealed class ConversionBinding
 {

src/Borm/Properties/Strings.Designer.cs
@@ -37,6 +37,14 @@ public static class Strings
     }
 
     /// <summary>
+    /// Validation failed for entity type '{entityType}'.
+    /// </summary>
+    public static string EntityValidationFailed(Type entityType)
+    {
+        return GetString([entityType]);
+    }
+
+    /// <summary>
     /// Column index cannot be lesser than zero.
     /// </summary>
     public static string InvalidColumnIndex()

src/Borm/Properties/Strings.resx
@@ -126,6 +126,9 @@
   <data name="EntityTypeCannotBeAbstract" xml:space="preserve">
     <value>The entity type '{0}' cannot be abstract.</value>
   </data>
+  <data name="EntityValidationFailed" xml:space="preserve">
+    <value>Validation failed for entity type '{0}'.</value>
+  </data>
   <data name="InvalidColumnIndex" xml:space="preserve">
     <value>Column index cannot be lesser than zero.</value>
   </data>

tests/Borm.Test/Data/Sql/ParameterBatchQueueTest.cs
@@ -1,0 +1,69 @@
+﻿using System.Data;
+using Borm.Data.Sql;
+using Moq;
+
+namespace Borm.Tests.Data.Sql;
+
+public sealed class ParameterBatchQueueTest
+{
+    [Fact]
+    public void AddFromRow_ReadsDataRowItemArrayIntoBatchQueue()
+    {
+        // Arrange
+        DataRow row = CreateTestRow();
+
+        ParameterBatchQueue queue = new();
+
+        // Act
+        queue.AddFromRow(row);
+
+        // Assert
+        Assert.Equal(1, queue.Count);
+        Assert.True(queue.Next());
+    }
+
+    [Fact]
+    public void SetDbParameters_ShouldAssignValuesToCommandParameters()
+    {
+        // Arrange
+        int id = 1;
+        string name = "Alice";
+
+        ParameterBatchQueue queue = new();
+        DataRow row = CreateTestRow();
+        queue.AddFromRow(row);
+
+        Mock<IDbCommand> mockCommand = new();
+        Mock<IDataParameterCollection> mockParams = new();
+
+        Mock<IDbDataParameter> param1 = new();
+        Mock<IDbDataParameter> param2 = new();
+
+        List<IDbDataParameter> @params = [param1.Object, param2.Object];
+
+        mockParams.Setup(p => p[It.IsAny<int>()]).Returns<int>(i => @params[i]);
+        mockCommand.Setup(c => c.Parameters).Returns(mockParams.Object);
+
+        queue.Next(); // Advance to next position
+
+        // Act
+        queue.SetParameterValues(mockCommand.Object);
+
+        // Assert
+        param1.VerifySet(p => p.Value = id, Times.Once);
+        param2.VerifySet(p => p.Value = name, Times.Once);
+    }
+
+    private static DataRow CreateTestRow()
+    {
+        DataTable table = new();
+        table.Columns.Add("id", typeof(int));
+        table.Columns.Add("name", typeof(string));
+
+        DataRow row = table.NewRow();
+        row["id"] = 1;
+        row["name"] = "Alice";
+        table.Rows.Add(row);
+        return row;
+    }
+}

tests/Borm.Test/Data/Sql/SqlStatementTest.cs
@@ -8,89 +8,58 @@ namespace Borm.Tests.Data.Sql;
 public sealed class SqlStatementTest
 {
     [Fact]
-    public void PrepareCommand_ShouldSetCommandTextAndAddParameters()
+    public void Constructor_ShouldInitializeBatchQueue()
+    {
+        // Act
+        SqlStatement statement = new(string.Empty, []);
+
+        // Assert
+        Assert.NotNull(statement.BatchQueue);
+    }
+
+    [Fact]
+    public void Prepare_ShouldSetCommandTextAndAddParameters()
     {
         // Arrange
-        Mock<IDbCommand> mockCommand = new();
-        Mock<IDataParameterCollection> mockParams = new();
-
-        DbParameter[] parameters = [CreateParameter("$id", 1), CreateParameter("$name", "Alice")];
-
-        mockCommand.SetupAllProperties();
-        mockCommand.Setup(c => c.Parameters).Returns(mockParams.Object);
-
-        string sql = "INSERT INTO table VALUES($id, $name);";
+        string sql = "INSERT INTO person VALUES ($id, $name);";
+        DbParameter[] parameters =
+        [
+            CreateParameter("id", DbType.Int32),
+            CreateParameter("name", DbType.String),
+        ];
 
         SqlStatement statement = new(sql, parameters);
 
+        Mock<IDbCommand> mockCommand = new();
+        Mock<IDataParameterCollection> mockParams = new();
+
+        int addCalls = 0;
+
+        mockParams.Setup(p => p.Clear());
+        mockParams
+            .Setup(p => p.Add(It.IsAny<IDbDataParameter>()))
+            .Callback<object>(_ => addCalls++);
+
+        mockCommand.Setup(c => c.Parameters).Returns(mockParams.Object);
+        mockCommand.SetupProperty(c => c.CommandText);
+        mockCommand.Setup(c => c.Prepare());
+
         // Act
-        statement.PrepareCommand(mockCommand.Object);
+        statement.Prepare(mockCommand.Object);
 
         // Assert
-        mockCommand.VerifySet(c => c.CommandText = sql);
-        mockParams.Verify(c => c.Add(It.IsAny<object>()), Times.Exactly(2));
+        Assert.Equal(sql, mockCommand.Object.CommandText);
+        mockParams.Verify(p => p.Clear(), Times.Once);
+        Assert.Equal(2, addCalls);
         mockCommand.Verify(c => c.Prepare(), Times.Once);
     }
 
-    [Fact]
-    public void SetParameters_ShouldSetValuesFromDataRow()
-    {
-        // Arrange
-        int id = 1;
-        string name = "Alice";
-
-        DataTable table = new();
-        table.Columns.Add("id", typeof(int));
-        table.Columns.Add("name", typeof(string));
-
-        DataRow row = table.NewRow();
-        row["id"] = id;
-        row["name"] = name;
-        table.Rows.Add(row);
-
-        // Missing parameter prefix is intentional
-        DbParameter[] parameters = [CreateParameter("id", null), CreateParameter("$name", null)];
-
-        SqlStatement statement = new("some", parameters);
-
-        // Act
-        statement.SetParameters(row);
-
-        // Assert
-        Assert.Equal(id, parameters[0].Value);
-        Assert.Equal(name, parameters[1].Value);
-    }
-
-    [Fact]
-    public void SetParameters_ThrowsInvalidOperationException_WhenColumnIsMissing()
-    {
-        // Arrange
-        int id = 1;
-
-        DataTable table = new();
-        table.Columns.Add("id", typeof(int));
-
-        DataRow row = table.NewRow();
-        row["id"] = id;
-        table.Rows.Add(row);
-
-        DbParameter[] parameters = [CreateParameter("$id", null), CreateParameter("$name", null)];
-
-        SqlStatement statement = new("some", parameters);
-
-        // Act
-        Exception exception = Record.Exception(() => statement.SetParameters(row));
-
-        // Assert
-        Assert.IsType<InvalidOperationException>(exception);
-    }
-
-    private static DbParameter CreateParameter(string name, object? value)
+    private static DbParameter CreateParameter(string name, DbType type)
     {
         Mock<DbParameter> mockParam = new();
         mockParam.SetupAllProperties();
         mockParam.Object.ParameterName = name;
-        mockParam.Object.Value = value;
+        mockParam.Object.DbType = type;
         return mockParam.Object;
     }
 }

tests/Borm.Test/Model/Metadata/BindingInfoTest.cs
@@ -1,5 +1,98 @@
-﻿namespace Borm.Tests.Model.Metadata;
+﻿using Borm.Model.Metadata;
+
+namespace Borm.Tests.Model.Metadata;
+
 public sealed class BindingInfoTest
 {
+    [Fact]
+    public void CreateBinding_ShouldMaterialize_WithConstructorBinding()
+    {
+        // Arrange
+        int id = 1;
+        string name = "Alice";
 
+        ColumnInfo idColumn = new(0, "id", "Id", typeof(int), Constraints.PrimaryKey, null);
+        ColumnInfo nameColumn = new(1, "name", "Name", typeof(string), Constraints.None, null);
+
+        ColumnInfoCollection columns = new([idColumn, nameColumn]);
+
+        BindingInfo bindingInfo = new(typeof(PersonB), columns);
+        ConversionBinding binding = bindingInfo.CreateBinding();
+
+        ValueBuffer buffer = new();
+        buffer[idColumn] = id;
+        buffer[nameColumn] = name;
+
+        // Act
+        PersonB person = (PersonB)binding.MaterializeEntity(buffer);
+
+        // Assert
+        Assert.Equal(id, person.Id);
+        Assert.Equal(name, person.Name);
+    }
+
+    [Fact]
+    public void CreateBinding_ShouldMaterializeAndConvert_WithPropertySetter()
+    {
+        // Arrange
+        int id = 1;
+        string name = "Alice";
+
+        ColumnInfo idColumn = new(0, "id", "Id", typeof(int), Constraints.PrimaryKey, null);
+        ColumnInfo nameColumn = new(1, "name", "Name", typeof(string), Constraints.None, null);
+
+        ColumnInfoCollection columns = new([idColumn, nameColumn]);
+
+        BindingInfo bindingInfo = new(typeof(PersonA), columns);
+        ConversionBinding binding = bindingInfo.CreateBinding();
+
+        ValueBuffer buffer = new();
+        buffer[idColumn] = id;
+        buffer[nameColumn] = name;
+
+        // Act
+        PersonA person = (PersonA)binding.MaterializeEntity(buffer);
+
+        // Assert
+        Assert.Equal(id, person.Id);
+        Assert.Equal(name, person.Name);
+    }
+
+    [Fact]
+    public void CreateBinding_ShouldMaterializeEntity_WithNullableColumn()
+    {
+        // Arrange
+        int id = 1;
+
+        ColumnInfo idColumn = new(0, "id", "Id", typeof(int), Constraints.PrimaryKey, null);
+        ColumnInfo nameColumn = new(1, "name", "Name", typeof(string), Constraints.AllowDbNull, null);
+
+        ColumnInfoCollection columns = new([idColumn, nameColumn]);
+
+        BindingInfo bindingInfo = new(typeof(PersonA), columns);
+        ConversionBinding binding = bindingInfo.CreateBinding();
+
+        ValueBuffer buffer = new();
+        buffer[idColumn] = id;
+        buffer[nameColumn] = DBNull.Value;
+
+        // Act
+        PersonA person = (PersonA)binding.MaterializeEntity(buffer);
+
+        // Assert
+        Assert.Equal(id, person.Id);
+        Assert.Null(person.Name);
+    }
+
+    public sealed class PersonA
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+
+    public sealed class PersonB(int id, string name)
+    {
+        public int Id { get; } = id;
+        public string Name { get; } = name;
+    }
 }