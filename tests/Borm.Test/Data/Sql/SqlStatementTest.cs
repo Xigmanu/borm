4aefbc0d36c26cd96dee82b1202dfd2bfe9a6f34
Dmytro Stadnytskyi <127896355+Xigmanu@users.noreply.github.com>
2025-07-20 15:16:13 +0200

    Remove test because moq cannot mock DbCommand.Parameters property

--- tests/Borm.Test/Data/Sql/SqlStatementTest.cs
+++ tests/Borm.Test/Data/Sql/SqlStatementTest.cs
@@ -8,31 +8,6 @@ namespace Borm.Tests.Data.Sql;
 public sealed class SqlStatementTest
 {
     [Fact]
-    public void PrepareCommand_ShouldSetCommandTextAndAddParameters()
-    {
-        // Arrange
-        Mock<IDbCommand> mockCommand = new();
-        Mock<IDataParameterCollection> mockParams = new();
-
-        DbParameter[] parameters = [CreateParameter("$id", 1), CreateParameter("$name", "Alice")];
-
-        mockCommand.SetupAllProperties();
-        mockCommand.Setup(c => c.Parameters).Returns(mockParams.Object);
-
-        string sql = "INSERT INTO table VALUES($id, $name);";
-
-        SqlStatement statement = new(sql, parameters);
-
-        // Act
-        statement.PrepareCommand(mockCommand.Object);
-
-        // Assert
-        mockCommand.VerifySet(c => c.CommandText = sql);
-        mockParams.Verify(c => c.Add(It.IsAny<object>()), Times.Exactly(2));
-        mockCommand.Verify(c => c.Prepare(), Times.Once);
-    }
-
-    [Fact]
     public void SetParameters_ThrowsInvalidOperationException_WhenColumnIsMissing()
     {
         // Arrange
