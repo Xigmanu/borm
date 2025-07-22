f5d2cc151b9d6fbbe7ccd7da1159011e074a6337
Dmytro Stadnytskyi <127896355+Xigmanu@users.noreply.github.com>
2025-07-22 13:16:42 +0200

    Add support for async transactions

--- src/Borm/Transaction.cs
+++ src/Borm/Transaction.cs
@@ -1,61 +1,110 @@
-﻿using Borm.Data;
-using System.Data;
+﻿using System.Data;
+using Borm.Data;
 
 namespace Borm;
 
-public sealed class Transaction : IDisposable
+public sealed class Transaction : IDisposable, IAsyncDisposable
 {
     private readonly DataContext _context;
     private readonly DataSet _dataSetCopy;
     private readonly bool _writeOnCommit;
-    private Exception? exception;
+    private bool _committed;
+    private Exception? _exception;
 
-    public Transaction(DataContext context, bool writeOnCommit)
+    internal Transaction(DataContext context, bool writeOnCommit)
     {
         _context = context;
         _writeOnCommit = writeOnCommit;
         _dataSetCopy = context.DataSet.Copy();
-        exception = null;
+        _exception = null;
+        _committed = false;
     }
 
-    public Exception? Exception => exception;
+    public Exception? Exception => _exception;
 
     public void Dispose()
     {
-        TryCommit();
-        _dataSetCopy.Dispose();
+        try
+        {
+            CommitInternal();
+        }
+        finally
+        {
+            _dataSetCopy.Dispose();
+        }
     }
 
-    internal void Execute(string tableName, Action<NodeDataTable> tableOp)
+    public async ValueTask DisposeAsync()
+    {
+        try
+        {
+            await CommitInternalAsync();
+        }
+        finally
+        {
+            _dataSetCopy.Dispose();
+        }
+    }
+
+    internal void Execute(string tableName, Action<NodeDataTable> tableOperation)
     {
         NodeDataTable tableCopy = GetTableCopy(tableName);
         try
         {
-            tableOp(tableCopy);
+            tableOperation(tableCopy);
         }
         catch (Exception ex)
         {
-            exception = ex;
+            _exception ??= ex;
+            _dataSetCopy.RejectChanges();
             throw;
+        }
+    }
+
+    private void CommitInternal()
+    {
+        if (_exception != null || _committed)
+        {
+            return;
+        }
+
+        _context.DataSet.Merge(_dataSetCopy, preserveChanges: true, MissingSchemaAction.Error);
+        if (_writeOnCommit)
+        {
+            _context.SaveChanges();
+        }
+
+        _committed = true;
+    }
+
+    private async Task CommitInternalAsync()
+    {
+        if (_exception != null || _committed || !_dataSetCopy.HasChanges())
+        {
+            return;
+        }
+
+        await _context.Semaphore.WaitAsync();
+        try
+        {
+            // FIXME
+            // If the same row is added by a different thread (NOT transaction) and written to the data source,
+            // preserveChanges will overwrite the DataRowState forcing it to be added to the parameter value queue again.
+            _context.DataSet.Merge(_dataSetCopy, preserveChanges: true, MissingSchemaAction.Error);
+
+            if (_writeOnCommit)
+            {
+                await _context.SaveChangesAsync();
+            }
+        }
+        finally
+        {
+            _context.Semaphore.Release();
         }
     }
 
     private NodeDataTable GetTableCopy(string tableName)
     {
         return (NodeDataTable)_dataSetCopy.Tables[tableName]!;
-    }
-
-    private void TryCommit()
-    {
-        if (exception != null)
-        {
-            return;
-        }
-
-        _context.DataSet.Merge(_dataSetCopy, true, MissingSchemaAction.Error);
-        if (_writeOnCommit)
-        {
-            _context.SaveChanges();
-        }
     }
 }
