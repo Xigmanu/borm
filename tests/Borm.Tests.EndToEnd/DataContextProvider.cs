8de442a96291f8aee38ec2a5b0e16fa5fb1c72d7
Dmytro Stadnytskyi <127896355+Xigmanu@users.noreply.github.com>
2025-11-03 17:08:50 +0100

    Add builder logic to e2e data context

--- tests/Borm.Tests.EndToEnd/DataContextProvider.cs
+++ tests/Borm.Tests.EndToEnd/DataContextProvider.cs
@@ -1,6 +1,7 @@
 ﻿using Borm.Model;
 using Borm.Model.Construction;
 using Borm.Tests.Common;
+using Borm.Tests.EndToEnd.Entities;
 
 namespace Borm.Tests.EndToEnd;
 
@@ -10,6 +11,13 @@ internal static class DataContextProvider
     {
         List<EntityInfo> model =
         [
+            new EntityBuilder<DebugEntity>()
+                .Name("debug_infos")
+                .Column(b => b.Index(0).Mapping(e => e.Id).PrimaryKey())
+                .Column(b => b.Index(1).Mapping("begin_date", e => e.Begin))
+                .Column(b => b.Index(2).Mapping("end_date", e => e.End))
+                .Column(b => b.Index(3).Mapping("meta", e => e.Metadata))
+                .Build(),
             EntityFactory.FromType(typeof(AddressEntity), new AddressEntity.Validator()),
             EntityFactory.FromType(typeof(PersonEntity)),
             EntityFactory.FromType(typeof(EmployeeEntity)),
