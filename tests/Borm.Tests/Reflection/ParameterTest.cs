80e9339f13b4848a5fa52212d0f79f7af40bce7f
Dmytro Stadnytskyi <127896355+Xigmanu@users.noreply.github.com>
2025-12-14 19:23:04 +0100

    Add final batch of tests and refactors

---
+++ tests/Borm.Tests/Reflection/ParameterTest.cs
@@ -0,0 +1,24 @@
+﻿using Borm.Reflection;
+using Borm.Reflection.Internal;
+
+namespace Borm.Tests.Reflection;
+
+public sealed class ParameterTest
+{
+    [Fact]
+    public void Constructor_InitializesNewInstanceAndSetsProperties()
+    {
+        // Arrange
+        string memberName = "name";
+        NullableType dataType = new(typeof(int), false);
+
+        // Act
+        Parameter parameter = new(memberName, dataType);
+
+        // Assert
+        Assert.Equal(memberName, parameter.MemberName);
+        Assert.Equal(dataType, parameter.DataType);
+        Assert.Null(parameter.Mapping);
+        Assert.Null(parameter.ValidationInfo);
+    }
+}
---
+++ tests/Borm.Tests/Reflection/PropertyTest.cs
@@ -0,0 +1,27 @@
+﻿using Borm.Model;
+using Borm.Reflection;
+using Borm.Reflection.Internal;
+
+namespace Borm.Tests.Reflection;
+
+public sealed class PropertyTest
+{
+    [Fact]
+    public void Constructor_InitializesNewInstanceAndSetsProperties()
+    {
+        // Arrange
+        string memberName = "name";
+        NullableType dataType = new(typeof(int), false);
+        MappingInfo mapping = new(0, null, false, false, null, ReferentialAction.NoAction);
+        ValidationInfo? validation = null;
+
+        // Act
+        Property property = new(memberName, dataType, mapping, validation);
+
+        // Assert
+        Assert.Equal(memberName, property.MemberName);
+        Assert.Equal(dataType, property.DataType);
+        Assert.Equal(mapping, property.Mapping);
+        Assert.Equal(validation, property.ValidationInfo);
+    }
+}
